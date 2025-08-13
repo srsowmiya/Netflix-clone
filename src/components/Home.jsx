2d804d4e9f31c10a5230e200daecdcfb7f90b9a0
srsowmiya <sowmiyasundaram1040@gmail.com>
2025-08-13 22:03:11 +0530

    homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,19 @@ import Navbar from './Navbar'
 
 const Home = () => {
   return (
-    <div className=''>
+    <>
+   
+     <div className=''>
        <Navbar/>
+       <div className="hero">
+        <img src="/homeScreentesting1.jpg" alt="" className='h-screen object-cover w-full'></img>   
+        <div>
+         <img src="/title.png" alt="" className='' />
+          <p>To All the Boys I've Loved Before is a teen romantic comedy about Lara Jean, whose secret love letters to past crushes are accidentally sent out, turning her quiet high school life into a whirlwind of awkward, funny, and heartwarming moments.</p>
+        </div>
+        </div>
     </div>
+ </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { FaBell } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
-
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FaBell, FaUser } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="mx-3 my-2 flex flex-row items-center justify-between h-15 bg-black text-white font-bold">
-  {/* Left items */}
-  <div className="flex flex-row items-center gap-5">
-    <img src="/netflix-logo.jpg" alt="" className="h-11" />
-    <a href="">Home</a>
-    <a href="">Tv Series</a>
-    <a href="">Movies</a>
-    <a href="">New & Popular</a>
-    <a href="">My List</a>
-    <a href="">Browse By Languages</a>
-  </div>
+    <nav id="navbar" class="fixed top-0 left-0 w-full z-50 transition-colors duration-300 bg-transparent">
+      <div className="w-full px-8 flex items-center justify-between h-16">
+        <div className="flex items-center gap-6">
+          <img src="/Netflix1-logo.png" alt="Netflix Logo" className="h-14" />
+          <a href="#" className="hover:text-gray-300">Home</a>
+          <a href="#" className="hover:text-gray-300">TV Series</a>
+          <a href="#" className="hover:text-gray-300">Movies</a>
+          <a href="#" className="hover:text-gray-300">New & Popular</a>
+          <a href="#" className="hover:text-gray-300">My List</a>
+          <a href="#" className="hover:text-gray-300">Browse by Languages</a>
+        </div>
+        <div className="flex items-center gap-6">
+          <a href="#"><FontAwesomeIcon icon={faMagnifyingGlass} className="hover:text-gray-300" /></a>
+          <a href="#" className="hover:text-gray-300">Children</a>
+          <a href="#"><FaBell className="hover:text-gray-300" /></a>
+          <a href="#"><FaUser className="hover:text-gray-300" /></a>
+        </div>
 
-  {/* Right items */}
-  <div className="flex flex-row items-center gap-5">
-    <a href=""><FontAwesomeIcon icon={faMagnifyingGlass} /></a>
-    <a href="">Children</a>
-    <a href=""><FaBell /></a>
-    <a href=""><FaUser /></a>
-  </div>
-</div>
+      </div>
+  
+    </nav>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
